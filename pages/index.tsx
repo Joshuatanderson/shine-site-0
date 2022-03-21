5ad27c2b460ec84157314b18731ed31807df304e
Joshua Anderson <joshuatanderson1@gmail.com>
2022-03-21 15:52:23 -0500

    feat: adding why shine section

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,18 +16,24 @@ import {
     Typography,
 } from "@mui/material";
 import { darkTheme, lightTheme, yellowTheme } from "../styles/theme";
-import CloudIcon from "@mui/icons-material/Cloud";
 import {
-    CloudCircleOutlined,
     CloudOutlined,
     PersonOutline,
+    SearchOutlined,
+    CurrencyBitcoinOutlined,
+    SolarPowerOutlined,
 } from "@mui/icons-material";
-import { jsx } from "@emotion/react";
+import ReactPlayer from "react-player";
+
+// import explainerVid from "../public/explainer.mov";
 
 const Home: NextPage = () => {
     const classes = {
         icon: {
             fontSize: 80,
+        },
+        whiteUnderline: {
+            textDecorationColor: lightTheme.palette.common.white,
         },
     };
 
@@ -144,24 +150,145 @@ const Home: NextPage = () => {
                         <Grid container spacing={3}>
                             <Grid item xs={12} sm={6}>
                                 <Typography variant="h3">
-                                    ShineMine is the better way to mine
+                                    <Box
+                                        component="u"
+                                        sx={classes.whiteUnderline}
+                                    >
+                                        ShineMine
+                                    </Box>{" "}
+                                    is the better way to mine
                                 </Typography>
                                 <Typography variant="body2">
-                                    ShineMine breaks down all of these barriers
-                                    and allows anyone to get involved with
-                                    crypto mining. ShineMine is a tokenized
-                                    crypto mining operation that allows anyone
-                                    to reap the profits of crypto mining at any
-                                    price point regardless of their knowledge or
-                                    experience with crypto. All someone needs to
-                                    do is hold SHINE tokens to participate. Once
-                                    SHINE tokens are purchased, the benefits of
-                                    crypto mining begin, without the drawbacks.
+                                    ShineMine breaks down barriers and allows
+                                    anyone to get involved with crypto mining.
                                 </Typography>
+                                <Typography variant="body2">
+                                    ShineMine is a tokenized crypto mining
+                                    operation that allows anyone to reap the
+                                    profits of crypto mining at any price point
+                                    regardless of their knowledge or experience
+                                    with crypto.
+                                </Typography>
+                                <Typography variant="body2">
+                                    All someone needs to do is hold SHINE tokens
+                                    to participate. Once SHINE tokens are
+                                    purchased, the benefits of crypto mining
+                                    begin, without the drawbacks.
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                {/* <ReactPlayer src={explainerVid}></ReactPlayer> */}
                             </Grid>
                         </Grid>
                     </Container>
                 </ScopedCssBaseline>
+                <ThemeProvider theme={lightTheme}>
+                    <ScopedCssBaseline>
+                        <Container
+                            sx={{ marginTop: "48px", marginBottom: "48px" }}
+                        >
+                            <Grid container spacing={3}>
+                                <Grid item container justifyContent="center">
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                    >
+                                        Buy Shine
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                    >
+                                        View Whitepaper
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Container>
+                    </ScopedCssBaseline>
+                </ThemeProvider>
+                <ThemeProvider theme={darkTheme}>
+                    <ScopedCssBaseline>
+                        <Container
+                            sx={{ marginTop: "48px", paddingBottom: "48px" }}
+                        >
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant="h2" color="secondary">
+                                        Why Shine
+                                    </Typography>
+                                </Grid>
+                                <Grid container item xs={12} sm={6} spacing={3}>
+                                    <Grid container item xs={12} spacing={3}>
+                                        <Grid item xs={3}>
+                                            <CurrencyBitcoinOutlined
+                                                sx={classes.icon}
+                                            />
+                                        </Grid>
+                                        <Grid item xs>
+                                            <Typography
+                                                variant="h6"
+                                                color="secondary"
+                                            >
+                                                No Minimum
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="secondary"
+                                            >
+                                                Start crypto mining at any price
+                                                point
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container item xs={12} spacing={3}>
+                                        <Grid item xs={3}>
+                                            <SearchOutlined
+                                                sx={classes.icon}
+                                            ></SearchOutlined>
+                                        </Grid>
+                                        <Grid item xs>
+                                            <Typography
+                                                variant="h6"
+                                                color="secondary"
+                                            >
+                                                Transparent
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="secondary"
+                                            >
+                                                Activities and team are public
+                                                and its code is open source
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container item xs={12} spacing={3}>
+                                        <Grid item xs={3}>
+                                            <SolarPowerOutlined
+                                                sx={classes.icon}
+                                            />
+                                        </Grid>
+                                        <Grid item xs container>
+                                            <Typography
+                                                variant="h6"
+                                                color="secondary"
+                                            >
+                                                Carbon neutral
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="secondary"
+                                            >
+                                                Solves the problem of the high
+                                                heat mining produces
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Container>
+                    </ScopedCssBaseline>
+                </ThemeProvider>
             </ThemeProvider>
         </>
     );
