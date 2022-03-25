04b6ef9b0091068edd581d40c45d88f2d2bad090
Joshua Anderson <joshuatanderson1@gmail.com>
2022-03-24 23:26:55 -0500

    feat: social buttons for title bar

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -1,7 +1,16 @@
-import { Container, Grid, ScopedCssBaseline, Typography } from "@mui/material";
+import { Instagram, Twitter } from "@mui/icons-material";
+import {
+    Container,
+    Grid,
+    ScopedCssBaseline,
+    SvgIcon,
+    Typography,
+} from "@mui/material";
 import { ThemeProvider } from "@mui/private-theming";
 import React, { Children, Component } from "react";
+import { FaDiscord } from "react-icons/fa";
 import { darkTheme } from "../styles/theme";
+import Link from "./Link";
 
 interface TitleBarProps {
     title: string;
@@ -11,6 +20,12 @@ interface TitleBarProps {
 const classes = {
     title: {
         fontSize: "4rem",
+    },
+    icon: {
+        fontSize: 20,
+    },
+    discordIcon: {
+        fontSize: 22,
     },
 };
 
@@ -22,6 +37,31 @@ const TitleBar = ({ title, children }: TitleBarProps) => {
                     <Typography variant="h1" align="center" sx={classes.title}>
                         {title}
                     </Typography>
+                </Grid>
+                <Grid
+                    container
+                    item
+                    xs={1}
+                    flexDirection="column"
+                    sx={{ position: "absolute", right: 20 }}
+                >
+                    <Link
+                        target="_blank"
+                        href="https://twitter.com/ShineMine_io"
+                    >
+                        <Twitter sx={classes.icon} />
+                    </Link>
+                    <Link
+                        target="_blank"
+                        href="https://www.instagram.com/shinemine.io/"
+                    >
+                        <Instagram sx={classes.icon} />
+                    </Link>
+                    <Link target="_blank" href="https://discord.gg/exsBtmkpxh">
+                        <SvgIcon sx={classes.discordIcon}>
+                            <FaDiscord />
+                        </SvgIcon>
+                    </Link>
                 </Grid>
                 {children}
             </Grid>
