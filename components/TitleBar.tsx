fbb7c86640f1c8033143a5eecb94adf00293702b
Joshua Anderson <joshuatanderson1@gmail.com>
2022-03-24 21:21:36 -0500

    fix: prevent titlebar overflow

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -8,12 +8,18 @@ interface TitleBarProps {
     children?: any;
 }
 
+const classes = {
+    title: {
+        fontSize: "4rem",
+    },
+};
+
 const TitleBar = ({ title, children }: TitleBarProps) => {
     return (
         <Container sx={{ paddingBottom: "48px" }}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                    <Typography variant="h1" align="center">
+                    <Typography variant="h1" align="center" sx={classes.title}>
                         {title}
                     </Typography>
                 </Grid>
