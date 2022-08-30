96bea3555d835c66a55f5c26dc81e562523624b1
Joshua Anderson <joshuatanderson1@gmail.com>
2022-08-30 13:07:49 -0500

    feat: attempting to add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@fontsource/roboto";
 import "@fontsource/bebas-neue";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { Box, ThemeProvider } from "@mui/system";
 import { lightTheme } from "../styles/theme";
 import Footer from "../components/Footer";
+import { pageview } from "../lib/ga";
 
 const classes = {
     app: {
@@ -15,6 +18,21 @@ const classes = {
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            pageview(url);
+        };
+        //When the component is mounted, subscribe to router changes
+        //and log those page views
+        router.events.on("routeChangeComplete", handleRouteChange);
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
     return (
         <>
             <CssBaseline>
